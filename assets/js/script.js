bee2cd4cb3dc1c82fa4d9f51dd89913c5c5a949d
MorCosta <118310093+MorCosta@users.noreply.github.com>
2023-01-30 14:32:33 +0000

    created the html code and part of the java sctip code

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  var currentDay = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(currentDay);
+
+  let currentHour = moment().hours();
+  $(".time-block").each(function() {
+    let blockHour = parseInt($(this).attr("id"));
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+
+  // Retrieve saved events from local storage and populate text areas
+  for (let i = 9; i < 18; i++) {
+    let event = localStorage.getItem(i.toString());
+    $("#" + i + " .calendar-item").val(event);
+  }
+
+  $(".saveBtn").on("click", function() {
+    let timeSlot = $(this).siblings(".time-slot").find(".calendar-item");
+    let event = timeSlot.val();
+    let hour = $(this).siblings(".time-block").attr("id");
+    localStorage.setItem(hour, event);
+  });
+});
+
+
